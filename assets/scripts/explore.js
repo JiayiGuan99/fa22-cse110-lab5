0ac9c4328f984fad6be8a32765773e1693fa4d90
Hazel <54999364+JiayiGuan99@users.noreply.github.com>
2022-10-30 18:17:49 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,35 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  let voices = [];
+  const voiceSelect = document.getElementById('voice-select');
+  const img = document.querySelector('img');
+  const text = document.getElementById("text-to-speak");
+  const button = document.querySelector('button');
+
+  synth.addEventListener('voiceschanged', () => {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`; // display
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  });
+
+  button.addEventListener('click', () => {
+    const inputText = new SpeechSynthesisUtterance(text.value);
+    synth.speak(inputText);
+    if(synth.speaking){
+      face.src = "assets/images/smiling-open.png";
+    }
+    else {
+      image.src = "assets/images/smiling.png";
+    }
+  });
 }
